7d13ab01b312e9ec3e219fc88a623ffa0ee0c056
jonhewitt <jchewitt.jh@gmail.com>
2018-10-19 14:39:58 -0500

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,8 @@
+const ROOT = 'https://angular-exploratory-api.herokuapp.com';
 export const environment = {
   production: true,
-  authority: 'http://localhost:3000/auth/login',
-  authority_check_url: 'http://localhost:3000/auth/check',
-  root_api_url: 'http://localhost:3000/api'
+  authority: `${ROOT}/auth/login`,
+  authority_check_url: `${ROOT}/auth/check`,
+  root_api_url: `${ROOT}/api`,
+  user_service: `${ROOT}/users`
 };
